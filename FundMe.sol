af3ed4758c0fd5fbd840fbcdf7cbc8af82fd1279
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2025-01-05 22:39:54 -0600

    convert eth to usd for minimum fund amount

--- FundMe.sol
+++ FundMe.sol
@@ -4,14 +4,22 @@ pragma solidity ^0.8.18;
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
+    uint public minimumUsd = 5e18;
+
     function fund() public payable {
-        require(msg.value > 1e18, "didn't send enough ETH");
+        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough ETH");
     }
 
-    function getPrice() public view returns(uint256) {
+    function getPrice() public view returns(uint) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price,,,) = priceFeed.latestRoundData();
+        (, int price,,,) = priceFeed.latestRoundData();
 
-        return uint256(price * 1e10);
+        return uint(price * 1e10);
+    }
+
+    function getConversionRate(uint ethAmount) public view returns(uint) {
+        uint ethPrice = getPrice();
+        uint ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 }
