538a28a68fbdf5fbdf8fc47506338927c30cdb58
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2025-01-05 22:29:42 -0600

    get eth/usd price from chainlink data feed

--- FundMe.sol
+++ FundMe.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     function fund() public payable {
         require(msg.value > 1e18, "didn't send enough ETH");
+    }
+
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+
+        return uint256(price * 1e10);
     }
 }
