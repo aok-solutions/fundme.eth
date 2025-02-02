a327be1cd11253e18f9fc173866ecd660cf5611c
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2025-02-01 21:32:43 -0600

    withdraw funds

--- FundMe.sol
+++ FundMe.sol
@@ -16,4 +16,21 @@ contract FundMe {
         funders.push(msg.sender);
         addressToFundAmount[msg.sender] = addressToFundAmount[msg.sender] + msg.value;
     }
+
+    function withdraw() public {
+        for(uint funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToFundAmount[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+//        payable(msg.sender).transfer(address(this).balance); // transfer
+
+//        bool sendSuccess = payable(msg.sender).send(address(this).balance); // send
+//        require(sendSuccess, "Send failed");
+
+        (bool callSuccess,) = payable(msg.sender).call{ value: address(this).balance }(""); // call
+        require(callSuccess, "Call failed");
+    }
 }
